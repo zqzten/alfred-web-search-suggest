998fbc17fd8623f3f33037feeff79b318413d4ce
Ziqiu Zhu <ZZQSHU@126.com>
2017-08-30 13:26:37 +0800

    improved Zhihu search suggestion

--- src/zhihu.php
+++ src/zhihu.php
@@ -18,36 +18,48 @@ $json = json_decode(mb_convert_encoding($response, 'UTF-8', 'HTML-ENTITIES'))[0]
 
 foreach ($json as $sugg) {
     if (is_array($sugg)) {
+        $matched = TRUE;
         $type = $sugg[0];
         switch ($type) {
             case 'topic':
                 $title = $sugg[1];
-                $subtitle = '【话题】'.$sugg[6].' 个精华问答';
-                $arg = $type.'/'.$sugg[2];
+                $subtitle = '【话题】'.$sugg[6].' 个精选回答';
+                $arg = "https://www.zhihu.com/".$type.'/'.$sugg[2];
                 $icon = saveAndReturnFile(str_replace('_s', '_m', $sugg[3]));
                 break;
             case 'people':
                 $title = $sugg[1];
                 $subtitle = '【用户】'.$sugg[5];
-                $arg = $type.'/'.$sugg[2];
+                $arg = "https://www.zhihu.com/".$type.'/'.$sugg[2];
                 $icon = saveAndReturnFile(str_replace('_s', '_m', $sugg[3]));
                 break;
             case 'question':
                 $title = $sugg[1];
-                $subtitle = '【内容】'.$sugg[4].' 个回答';
-                $arg = $type.'/'.$sugg[3];
+                $subtitle = '【问题】'.$sugg[4].' 个回答';
+                $arg = "https://www.zhihu.com/".$type.'/'.$sugg[3];
                 $icon = ICON;
+                break;
+            case 'article':
+                $title = $sugg[1];
+                $subtitle = '【文章】'.$sugg[4].' 个赞';
+                $arg = "https://zhuanlan.zhihu.com/p/".$sugg[3];
+                $icon = ICON;
+                break;
+            default:
+                $matched = FALSE;
                 break;
         }
 
-        $wf->result()
-            ->title("$title")
-            ->subtitle("$subtitle")
-            ->arg("$arg")
-            ->icon("$icon")
-            ->autocomplete("$title")
-            ->text('copy', "$title")
-            ->quicklookurl("https://www.zhihu.com/".$arg);
+        if ($matched) {
+            $wf->result()
+                ->title("$title")
+                ->subtitle("$subtitle")
+                ->arg("$arg")
+                ->icon("$icon")
+                ->autocomplete("$title")
+                ->copy("$title")
+                ->quicklookurl("$arg");
+        }
     }
 }
 
