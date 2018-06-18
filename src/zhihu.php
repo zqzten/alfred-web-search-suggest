2ef2cacd4e1db1a2bc78757401c5c52ab96fe7a4
Ziqiu Zhu <zzqshu@126.com>
2018-06-18 15:15:08 +0800

    fixed #10

--- src/zhihu.php
+++ src/zhihu.php
@@ -14,33 +14,30 @@ $download_dir = getenv('alfred_workflow_cache').'/zhihu';
 initDownloadDir(true);
 
 $response = request('https://www.zhihu.com/autocomplete?token='.urlencode($query));
-$json = json_decode(html_entity_decode($response, ENT_QUOTES | ENT_HTML5))[0];
+$json = json_decode($response)[0];
 
 foreach ($json as $sugg) {
     if (is_array($sugg)) {
         $matched = true;
         $type = $sugg[0];
+        $title = html_entity_decode($sugg[1], ENT_QUOTES | ENT_HTML5);
         switch ($type) {
             case 'topic':
-                $title = $sugg[1];
                 $subtitle = '【话题】'.$sugg[6].' 个精选回答';
                 $arg = 'https://www.zhihu.com/'.$type.'/'.$sugg[2];
                 $icon = saveAndReturnFile(str_replace('_s', '_m', $sugg[3]));
                 break;
             case 'people':
-                $title = $sugg[1];
-                $subtitle = '【用户】'.$sugg[5];
+                $subtitle = '【用户】'.html_entity_decode($sugg[5], ENT_QUOTES | ENT_HTML5);
                 $arg = 'https://www.zhihu.com/'.$type.'/'.$sugg[2];
                 $icon = saveAndReturnFile(str_replace('_s', '_m', $sugg[3]));
                 break;
             case 'question':
-                $title = $sugg[1];
                 $subtitle = '【问题】'.$sugg[4].' 个回答';
                 $arg = 'https://www.zhihu.com/'.$type.'/'.$sugg[3];
                 $icon = ICON;
                 break;
             case 'article':
-                $title = $sugg[1];
                 $subtitle = '【文章】'.$sugg[4].' 个赞';
                 $arg = 'https://zhuanlan.zhihu.com/p/'.$sugg[3];
                 $icon = ICON;
